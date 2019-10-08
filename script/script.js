ffa283ab3ff6f5c0eedf53f09c1cc03b84a005b5
Maslory <stanlisword@gmail.com>
2019-10-08 12:27:15 +0500

    adaptive

--- script/script.js
+++ script/script.js
@@ -1,21 +1,54 @@
 var next1 = document.getElementById('next');
 var prev1 = document.getElementById('prev');
 var upper = document.getElementById('upper');
-
+let all_info_about_company = document.querySelectorAll('#displayBlockNoneSection1, #displayBlockNoneSection2, #displayBlockNoneSection3, #displayBlockNoneSection4, #displayBlockNoneSection5, #displayBlockNoneSection6, #displayBlockNoneSection7');
 window.onload = function() {
 
  };
 var logo = document.getElementById("logo");
 var headTags = document.getElementById("headTags");
 
+
+// window.addEventListener("resize", function() {
+//     console.log(innerWidth);
+//     console.log(innerHeight);
+//     if(document.documentElement.clientWidth < 806){
+//         section1.style.display = "block";
+//         section2.style.display = "block";
+//         section3.style.display = "block";
+//         section4.style.display = "block";
+//         section5.style.display = "block";
+//         section6.style.display = "block";
+//         section7.style.display = "block";
+//        }
+//        if(document.documentElement.clientWidth > 806){
+//         section1.style.display = "none";
+//         section2.style.display = "none";
+//         section3.style.display = "none";
+//         section4.style.display = "none";
+//         section5.style.display = "none";
+//         section6.style.display = "none";
+//         section7.style.display = "none";
+//        }
+// }, false);
+
+
+
 $(window).on("scroll", function () {
     var scrolled = $(this).scrollTop();
-    if( scrolled > 30 ) {
+    if(document.documentElement.clientWidth < 1030) {
+        $('#header').css({'height' :'180px'});
+    }
+    if(document.documentElement.clientWidth > 1030) {
+        $('#header').css({'height' :'90px'});
+    }
+    if( scrolled > 30 && document.documentElement.clientWidth > 1030) {
         $('.content').addClass('scrolled');
         logo.setAttribute("src","img/logo2.png");
         $('.headTags').css({'color': '#4B1C1C'});
         $('.header1').css({'color': '#4B1C1C'});
         $('#upper').css({'display' :'block'});
+        
 
     }
     if( scrolled <= 30 ) {
@@ -41,11 +74,15 @@ var zakaz = document.getElementById('showForZakaz');
 var arch = document.getElementById('showForArch');
 var build = document.getElementById('showForCompany');
 var dekor1 = document.getElementById('dekor1');
-
+let buttonA1 = document.getElementById('buttonsA1');
+let buttonA2 = document.getElementById('buttonsA2');
+let buttonA3 = document.getElementById('buttonsA3');
 
 
  function showForZakaz() {
-   dekor1.style.marginLeft = '-5%';
+    buttonA1.style.display = 'block';
+    buttonA2.style.display = 'none';
+    buttonA3.style.display = 'none';
    zakaz.style.color = "#4B1C1C";
    arch.style.color = "#777777";
    build.style.color = "#777777";
@@ -60,7 +97,9 @@ var dekor1 = document.getElementById('dekor1');
  }
 
  function showForArch() {
-    dekor1.style.marginLeft = '95%';
+    buttonA1.style.display = 'none';
+    buttonA2.style.display = 'block';
+    buttonA3.style.display = 'none';
    zakaz.style.color = "#777777";
    arch.style.color = "#4B1C1C";
    build.style.color = "#777777";
@@ -75,7 +114,9 @@ var dekor1 = document.getElementById('dekor1');
  }
 
  function showForCompany() {
-    dekor1.style.marginLeft = '210%';
+    buttonA1.style.display = 'none';
+    buttonA2.style.display = 'none';
+    buttonA3.style.display = 'block';
    zakaz.style.color = "#777777";
    arch.style.color = "#777777";
    build.style.color = "#4B1C1C";
